e267a544d75ef68139271ca12999f8f2e848b148
Warren John Caruana <warrencaruana1@gmail.com>
2025-09-29 20:02:40 +0800

    Update web

--- src/lib/uploadImage.ts
+++ src/lib/uploadImage.ts
@@ -1,8 +1,13 @@
 import { decode } from "base64-arraybuffer";
 import * as FileSystem from "expo-file-system";
 import { supabase } from "./supabase";
+import { Platform } from "react-native";
 
 export async function uploadImage(uri: string, userId: string) {
+  if (Platform.OS === "web") {
+    return await uploadImageWeb(uri, userId);
+  }
+
   try {
     // Read the file into base64
     const base64 = await FileSystem.readAsStringAsync(uri, {
@@ -28,4 +33,30 @@ export async function uploadImage(uri: string, userId: string) {
     console.error("Upload error:", error);
     return null;
   }
+}
+
+async function uploadImageWeb(uri: string, userId: string) {
+  console.log("uri", uri);
+  const response = await fetch(uri);
+  console.log("response", response);
+  const blob = await response.blob();
+
+  const fileName = `timelogs/${userId}_${Date.now()}.jpg`;
+
+  console.log("filename", fileName);
+
+  const { data, error } = await supabase.storage
+    .from("timelog-photos")
+    .upload(fileName, blob, {
+      contentType: blob.type || "image/jpeg",
+      upsert: true,
+    });
+
+  if (error) {
+    console.error("Supabase upload error:", error);
+    throw error;
+  }
+
+  console.log("Uploaded file path:", data.path);
+  return data.path;
 }
